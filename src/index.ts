f60a0e4e9f75f2c93b5c0314aff79fbd24946ff0
Marcos Rodríguez <e.marcosra@gmail.com>
2024-03-21 14:26:49 +0100

    Add main TypeScript file

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+type Guitar = {
+  name: string;
+  color: string;
+  isElectricGuitar: boolean;
+};
+
+const guitars: Guitar[] = [
+  {
+    name: "Fender",
+    color: "black",
+    isElectricGuitar: true,
+  },
+  {
+    name: "Yamaha",
+    color: "black",
+    isElectricGuitar: false,
+  },
+];
